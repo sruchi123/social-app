8fad44ce936074f4474e37611640ce65399f7f66
Ruchi <ruchis@witmates.com>
2019-12-21 16:44:39 +0530

    force fully push

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
   def not_found
     render json: { error: 'not_found' }
   end
-  
+
   def authorize_request
     header = request.headers['Authorization']
     begin
